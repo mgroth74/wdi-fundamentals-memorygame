95dc73d20909d807068702213e05c3bccf7a5c22
Grothusen Michael <GrothusenMichaelW@JohnDeere.com>
2019-10-28 13:25:16 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,20 @@
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
+var cardOne = cards[0]
+var cardTwo = cards[1]
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("user flipped " + cardFour);
+console.log("User flipped " + cardTwo);
 
+if (cardsInPlay.length === 2)
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+
+	}else if (cardsInPlay[0] !== cardsInPlay[1]){
+		alert("Sorry, try again.");
+
+}
